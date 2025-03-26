f21b02a717b0fc8ca40d8513fd5d3612ea77c8c2
DaniilGazizullin <daniilfoot@bk.ru>
2025-03-27 01:56:52 +0300

    Add files via upload

---
+++ core1/src/task/oop/Employee.java
@@ -0,0 +1,23 @@
+package task.oop;
+
+public abstract class Employee {
+
+  protected String name;
+  protected int age;
+
+  public Employee(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public abstract void work();
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+}
---
+++ core1/src/task/oop/Main.java
@@ -0,0 +1,10 @@
+package task.oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager manager=new Manager("Виктор",22,"Технологии");
+        manager.work();
+        WareHouseWorker wareHouseWorker=new WareHouseWorker("dsaasd",123,"asdad");
+        wareHouseWorker.work();
+    }
+}
