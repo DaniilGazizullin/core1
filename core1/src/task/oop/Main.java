f47f9ccefb509dd6f5b928535dd96b8b14eeef26
DaniilGazizullin <daniilfoot@bk.ru>
2025-03-27 02:02:50 +0300

    Update Main.java

--- core1/src/task/oop/Main.java
+++ core1/src/task/oop/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Manager manager=new Manager("Виктор",22,"Технологии");
         manager.work();
-        WareHouseWorker wareHouseWorker=new WareHouseWorker("dsaasd",123,"asdad");
-        wareHouseWorker.work();
+        
     }
 }
