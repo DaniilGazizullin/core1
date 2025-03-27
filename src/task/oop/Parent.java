abc63265b7e7e055080498a2e96ef1bf5b824dbd
Даниил <daniilfoot@bk.ru>
2025-03-27 14:03:19 +0300

    Задания исправил

--- src/task/oop/Parent.java
+++ src/task/oop/Parent.java
@@ -3,22 +3,23 @@ package task.oop;
 public class Parent {
 
   public int sumOfTwoNumbers(int a, int b) {
-    return 0;
+    return a+b;
   }
 
   public int sumOfStringAndNumber(String str, int num) {
-    return 0;
+    int num1=Integer.parseInt(str);
+    return num+num1;
   }
 
   public String returnString(String str) {
-    return null;
+    return str;
   }
 
   public void printParent() {
-
+    System.out.println("I am Parent");
   }
 
   public String concatenateStrings(String str1, String str2) {
-    return null;
+    return str1+str2;
   }
 }
