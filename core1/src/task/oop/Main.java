413679c2aec81337091c481e1e465e6867787a45
Даниил <daniilfoot@bk.ru>
2025-03-28 14:53:42 +0300

    Задания исправил

--- core1/src/task/oop/Main.java
+++
@@ -1,9 +0,0 @@
-package task.oop;
-
-public class Main {
-    public static void main(String[] args) {
-        Manager manager=new Manager("Виктор",22,"Технологии");
-        manager.work();
-        
-    }
-}
